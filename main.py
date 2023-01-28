a3fa5881b27504859ed3380509ebdd85258d231d
Aleksandr <78909677+sudo-Cthulhufhtagn@users.noreply.github.com>
2023-01-28 17:58:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,8 @@ if __name__=='__main__':
             right_on='dt').drop(columns=['dt', 'noaa_scale'])
 
     message = tabulate(df_merged, headers='keys', tablefmt='psql',  showindex=False).replace("+",'*').replace('-','_')
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text=<pre>{message}</pre>"
+    parse_mode = 'html'
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text=<pre>{message}</pre>&parse_mode={parse_mode}"
     print(BOT_TOKEN, CHAT_ID)
     print(requests.get(url).json())
     print(message)
